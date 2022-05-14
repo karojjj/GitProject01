89fc93f5e0cd698c172e3a09a9dcb54532d8ffbc
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-14 12:46:04 -0400

    4th commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("bored");
         System.out.println("boring");
         System.out.println("feeling like ");
+        System.out.println("bla bla blaaaaa");
+        System.out.println("offffffffffffffffffffffff");
+        System.out.println("what a boring day " );
     }
 }
