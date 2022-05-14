0b59dd55066e4e5cf51a42a1896379eaaced0430
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-14 12:34:42 -0400

    3rd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,6 +3,8 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("bored");
+        System.out.println("boring");
+        System.out.println("feeling like ");
     }
 }
